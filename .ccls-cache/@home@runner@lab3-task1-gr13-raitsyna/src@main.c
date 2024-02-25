e3548ecc57cd14a20d6d3f1152100cb8654a128e
Yulia Raitsyna <137895986+yuliaraitsyna@users.noreply.github.com>
2024-02-25 13:40:53 +0000

    add task

--- .ccls-cache/@home@runner@lab3-task1-gr13-raitsyna/src@main.c
+++ .ccls-cache/@home@runner@lab3-task1-gr13-raitsyna/src@main.c
@@ -1,8 +1,17 @@
-/* main.c */
+#include "interface.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Usage: %s <number1> <number2> ...\n", argv[0]);
+    return 1;
+  }
+
+  for (int i = 1; i < argc; i++) {
+    int num = atoi(argv[i]);
+    rearrangeDigits(num);
+  }
+
+  return 0;
 }
